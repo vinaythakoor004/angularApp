f5edc269ca2e5d5f51f92b85b5a3273a78420f0e
vinaythakoor004 <vinaythakoor04@gmail.com>
2025-03-31 22:55:25 +0530

    Added pagination html

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -11,5 +11,49 @@ export class DashboardComponent {
   userData: Array<any> = [
     { firstName: "Satish", lastName: "Kumar", handle: "Twitter" },
     { firstName: "Ajay", lastName: "Patil", handle: "Facebook" },
-    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" }];
+    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" },
+    { firstName: "Satish", lastName: "Kumar", handle: "Twitter" },
+    { firstName: "Ajay", lastName: "Patil", handle: "Facebook" },
+    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" },
+    { firstName: "Satish", lastName: "Kumar", handle: "Twitter" },
+    { firstName: "Ajay", lastName: "Patil", handle: "Facebook" },
+    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" },
+    { firstName: "Satish", lastName: "Kumar", handle: "Twitter" },
+    { firstName: "Ajay", lastName: "Patil", handle: "Facebook" },
+    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" },
+    { firstName: "Satish", lastName: "Kumar", handle: "Twitter" },
+    { firstName: "Ajay", lastName: "Patil", handle: "Facebook" },
+    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" },
+    { firstName: "Satish", lastName: "Kumar", handle: "Twitter" },
+    { firstName: "Ajay", lastName: "Patil", handle: "Facebook" },
+    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" },
+    { firstName: "Satish", lastName: "Kumar", handle: "Twitter" },
+    { firstName: "Ajay", lastName: "Patil", handle: "Facebook" },
+    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" },
+    { firstName: "Satish", lastName: "Kumar", handle: "Twitter" },
+    { firstName: "Ajay", lastName: "Patil", handle: "Facebook" },
+    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" },
+    { firstName: "Satish", lastName: "Kumar", handle: "Twitter" },
+    { firstName: "Ajay", lastName: "Patil", handle: "Facebook" },
+    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" },
+    { firstName: "Satish", lastName: "Kumar", handle: "Twitter" },
+    { firstName: "Ajay", lastName: "Patil", handle: "Facebook" },
+    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" },
+    { firstName: "Satish", lastName: "Kumar", handle: "Twitter" },
+    { firstName: "Ajay", lastName: "Patil", handle: "Facebook" },
+    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" },
+    { firstName: "Satish", lastName: "Kumar", handle: "Twitter" },
+    { firstName: "Ajay", lastName: "Patil", handle: "Facebook" },
+    { firstName: "Rohan", lastName: "Datt", handle: "Twiiter" }    
+  ];
+  pageSize: Array<number> = [];
+
+  ngOninit() {
+     
+  }
+
+  getPageSize(): Array<number> {
+    let limit = Math.ceil(this.userData.length / 10);
+    return this.pageSize = [...Array(limit)].map((a, i) => i+1) 
+  }
 }
