dcc43c02224e8282c6502540cca6716248eaa279
vinaythakoor004 <vinaythakoor04@gmail.com>
2025-04-02 15:01:16 +0530

    Removed services comp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { ServicesComponent } from './services/services.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
@@ -10,7 +9,6 @@ export const routes: Routes = [
          children: [
             { path: '', redirectTo: '/home', pathMatch: 'full'},
             { path: 'home', component: HomeComponent },
-            { path: 'services', component: ServicesComponent },
             { path: 'projects', component: ProjectsComponent },
             { path: 'about', component: AboutComponent },
         ]
